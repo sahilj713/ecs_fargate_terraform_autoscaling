f278d6a77a1165462d9597b3e92e4059bdec6864
sahil jain <62873731+sahilj713@users.noreply.github.com>
2024-01-11 13:08:47 +0530

    updated

---
+++ role/dev/role.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "task_def_role" {
+  name = "ecs_role_test_2_task_def_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
